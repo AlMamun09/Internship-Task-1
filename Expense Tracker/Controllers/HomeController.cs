4ecaf6f8329304cc8131da036d73a79a3022e084
Abdullah Al Mamun <almamun2148@gmail.com>
2025-10-06 14:16:39 +0600

    Initial_Commit

---
+++ Expense Tracker/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expense_Tracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Education()
+        {
+            return View();
+        }
+        public IActionResult Skills()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
